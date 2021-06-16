ac2d766c33e28fd1f028580ebe2bd9aad87a2807
sakshi singh <1539sakshi@gmail.com>
2021-06-17 03:57:29 +0530

    THA-7

---
+++ DAY-7/question3.js
@@ -0,0 +1,7 @@
+var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12
+}
+objLength = Object.keys(student).length;
+console.log(objLength);
---
+++ DAY-7/question5.js
@@ -0,0 +1,10 @@
+const cylinder = {
+  pi: Math.PI,
+  radius: 10,
+  height: 10,
+  volume: function(){
+      return (this.pi * Math.pow(this.radius, 2) * this.height);
+  }
+}
+var vol = parseFloat( cylinder.volume().toFixed(4) );
+console.log(vol);
