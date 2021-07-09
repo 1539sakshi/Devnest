a04696c673b44f7fe9f1ab658c0d07bcee9c542a
sakshi singh <1539sakshi@gmail.com>
2021-07-10 00:14:55 +0530

    THA-16A

---
+++ Day-16/card/src/components/card.jsx
@@ -0,0 +1,15 @@
+import "./card.css"
+
+function Card() {
+  return (
+    <div className = "cont">
+    <div className = "card">
+      <img src  = "meme.jpeg" alt = " "/>
+      <h3>Meme Card</h3>
+      <p>This is a meme card</p>
+      </div>
+      </div>
+  );
+}
+
+export default Card;
